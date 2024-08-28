3b8e180c85f77856c8ffafcad12b73d707cbd1d8
claudiahW <murungiclaud@gmail.com>
2024-08-28 15:03:08 +0300

    style sections menu

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const sortBtns = document.querySelectorAll(".job-id > *");
+
+sortBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    sortBtns.forEach((btn) => btn.classList.remove("active"));
+    btn.classList.add("active");
+  });
+});
